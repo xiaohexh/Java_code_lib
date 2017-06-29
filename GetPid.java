1b6d725cafa502133007e0ab77f82a863a61d20c
chris.xiao <xiaohexh@hotmail.com>
2017-06-28 20:40:42 -0700

    add get local address get pid test false sharing(内存伪共享)
    Signed-off-by: chris.xiao <xiaohexh@hotmail.com>

---
+++ GetPid.java
@@ -0,0 +1,14 @@
+
+public class GetPid {
+	public static void main(String[] args) {
+
+	public int getPid() {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName(); // format: "pid@hostname"
+        try {
+            return Integer.parseInt(name.substring(0, name.indexOf('@')));
+        } catch (Exception e) {
+            return -1;
+        }
+	}
+}
